1bafc5b37ca9803206ae7a16b2854216330372fc
Dan Silaghi <dsilaghi@endava.net>
2016-11-23 13:46:26 +0200

    custom environment, attribute routing, tag helper, view imports, cache, action injection of dependencies

--- SampleProject/src/SampleProject/Controllers/HomeController.cs
+++ SampleProject/src/SampleProject/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using SampleProject.Models;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -13,10 +15,33 @@ namespace SampleProject.Controllers
     {
         [Route("[action]")]
         // GET: /<controller>/
-        public IActionResult Index()
+        public IActionResult Index([FromServices] IMemoryCache cache)
         {
-            ViewBag.Test = "test2";
-            return View();
+            DreamCars dc;
+            string cacheKey = "fullCarList";
+            if (cache.TryGetValue(cacheKey, out dc))
+            {
+                ViewBag.Title = "My favorite cars";
+                dc = new DreamCars();
+                dc.MyDreamCars = new List<Car>();
+                dc.MyDreamCars.Add(new Car("VW", "Golf"));
+                dc.MyDreamCars.Add(new Car("Skoda", "Octavia"));
+                dc.MyDreamCars.Add(new Car("BMW", "5 Series"));
+                dc.MyDreamCars.Add(new Car("Dacia", "Sandero"));
+
+                cache.Set(
+                    cacheKey,
+                    dc,
+                    new MemoryCacheEntryOptions().SetSlidingExpiration(TimeSpan.FromMinutes(10)));
+
+            }
+            return View(dc);
+        }
+
+        [HttpPost]
+        public IActionResult Details()
+        {
+            return Accepted();
         }
     }
 }
